20da9839b3fdd3298bb3499e3c47db42a506f1fa
Vinicius Assis <viniciusassis.dev@hotmail.com>
2023-11-02 10:40:01 -0300

    feat: create tours list endpoint

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours?.length || 0,
+    data: {
+      tours,
+    },
+  });
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App runing on port ${port}`);
+});
