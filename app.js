734b300b8c2d8806186a30281443e4067efd597f
Vinicius Assis <viniciusassis.dev@hotmail.com>
2023-11-13 15:35:07 -0300

    feat: create the delete structure method

--- app.js
+++ app.js
@@ -71,6 +71,22 @@ app.patch('api/v1/tours/:id', (req, res) => {
   });
 });
 
+app.delete('api/v1/tours/:id', (req, res) => {
+  const id = Number(req.params.id);
+
+  if (id > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`App runing on port ${port}`);
