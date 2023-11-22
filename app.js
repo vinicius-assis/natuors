f856f6f75b6892be0221aefa056959789e950389
Vinicius Assis <viniciusassis.dev@hotmail.com>
2023-11-22 05:53:34 -0300

    feat: use env variables to build route

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const toursRouter = require('./routes/tourRoutes');
 const usersRouter = require('./routes/userRoutes');
 
 const app = express();
-
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
