4857d694d5ce57e5dd3ca269d523f30479565e85
Vinicius Assis <viniciusassis.dev@hotmail.com>
2023-11-26 11:49:15 -0300

    feat: connect database in the project

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const app = require('./app');
 
 dotenv.config({
   path: './config.env',
 });
 
-const app = require('./app');
-
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB connection successfull!'));
 // console.log(process.env);
 
 const port = process.env.PORT || 3000;
