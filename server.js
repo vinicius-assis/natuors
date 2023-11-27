2b15a10cb8933e861338426455c780663b7506d5
Vinicius Assis <viniciusassis.dev@hotmail.com>
2023-11-27 19:12:46 -0300

    refactor: isolate your model to follow ther mvc architecture

--- server.js
+++ server.js
@@ -17,36 +17,6 @@ mongoose
     useFindAndModify: false,
   })
   .then(() => console.log('DB connection successfull!'));
-// console.log(process.env);
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name.'],
-    unique: true,
-  },
-  rating: {
-    type: Number,
-    default: 4.5,
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price.'],
-  },
-});
-
-const Tour = mongoose.model('Tour', tourSchema);
-
-const testTour = new Tour({
-  name: 'The Park Camper',
-  price: 997,
-});
-
-testTour
-  .save()
-  .then((doc) => {
-    console.log(doc);
-  })
-  .catch((err) => console.log('Error ===> ', err));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
