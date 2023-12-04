709247e8606e0b71458edba13e60c5bb4a6b1586
Vinicius Assis <viniciusassis.dev@hotmail.com>
2023-12-04 19:55:52 -0300

    feat: add tour sort param

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -9,7 +9,16 @@ const getAllTours = async (req, res) => {
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
 
-    const tours = await Tour.find(JSON.parse(queryStr));
+    let query = Tour.find(JSON.parse(queryStr));
+
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
+
+    const tours = await query;
 
     res.status(200).json({
       status: 'success',
